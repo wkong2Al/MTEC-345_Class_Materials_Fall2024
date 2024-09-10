1304f6012fff2c56d9fdde0ca3356683a5762c4d
Charles Holbrow <CharlesHolbrow@gmail.com>
2024-09-10 14:33:21 -0400

    extras: small updates

--- extras/interactive.py
+++ extras/interactive.py
@@ -1,6 +1,14 @@
 # %% 
-#  create a sine wave and put it in a numpy array
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 
+# VS Code commands (September 2024):
+# - ctrl+enter to run a cell in VS Code
+# - shift+enter to run a single/selected line(s) in a different interactive env
+# - command+shift+p to open the command palette
+#   - "install 'code' command in path"
+# - use `# %%` to separate "cells"
+
+# %%
+# create a 440 hz sine wav
